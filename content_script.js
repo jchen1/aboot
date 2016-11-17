9806efe49599b9dc157fd90b81ee8b7a334cf344
Jeff Chen <jeff@blendlabs.com>
2016-11-16 17:30:36 -0800

    handle cases

--- content_script.js
+++ content_script.js
@@ -1,19 +1,19 @@
 walk(document.body);
 
-function walk(node) 
+function walk(node)
 {
   // I stole this function from here:
   // http://is.gd/mwZp7E
 
   var child, next;
 
-  switch ( node.nodeType )  
+  switch ( node.nodeType )
   {
     case 1:  // Element
     case 9:  // Document
     case 11: // Document fragment
       child = node.firstChild;
-      while ( child ) 
+      while ( child )
       {
         next = child.nextSibling;
         walk(child);
@@ -32,8 +32,13 @@ function walk(node)
 function handleText(textNode) {
   var v = textNode.nodeValue;
 
-  v = v.replace(/about/g, 'aboot');
-  v = v.replace(/About/g, 'Aboot');
+  v = v.replace(/about/gi, function(match) {
+    if (match[3] === 'U') {
+      return match.substring(0, 3) + 'O' + match.substring(4);
+    } else {
+      return match.substring(0, 3) + 'o' + match.substring(4);
+    }
+  });
 
   textNode.nodeValue = v;
 }
